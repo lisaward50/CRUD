05c4a2a95bda576ccd63b0d3432c79723865dd81
lisaward50 <lisa.ward50@gmail.com>
2019-01-08 14:05:19 -0500

    initial routes and view structure

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get("/whiskies", function(req, res){
   res.render("index");
 });
 
+app.get("/whiskies/new", function(req, res){
+  res.render("new");
+});
+
+app.post("/whiskies", function(req, res){
+
+});
+
 app.listen(PORT, function(){
   console.log("Server is up and listening");
 });
