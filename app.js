a45bd9ab916c8d2e5120486d8eb31905eb9e1912
lisaward50 <lisa.ward50@gmail.com>
2019-01-09 14:34:53 -0500

    seed

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
 //resetDatabase();
-//seedDatabase();
+seedDatabase();
 
 app.get("/", function(req, res){
   res.render("home");
